bd77224d95a1a3359596b8894eede301c1d8b631
Marc Schwartz <m.s.schwartz@students.uu.nl>
2019-02-28 16:32:45 +0100

    Add models for counterparties

--- counterparties/models.py
+++ counterparties/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+
+class CounterpartyType(models.Model):
+    description = models.CharField(max_length=255)
+    token = models.CharField(max_length=127)
+
+    def __str__(self):
+        return self.description
+
+
+class Counterparty(models.Model):
+    name = models.CharField(max_length=255)
+    counter_party_type = models.ForeignKey(CounterpartyType, on_delete=models.PROTECT)
+
+    def __str__(self):
+        return '%s (%s)' % (self.name, CounterpartyType.description)
