9f7e3dc2ea6a8e8b1c3cc6ace07e9a4b303bcd03
Marc Schwartz <m.s.schwartz@students.uu.nl>
2019-03-04 17:33:22 +0100

    Define string representations for bank account models

--- bank_accounts/models.py
+++ bank_accounts/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 
+from classes.Iban import Iban
 from counterparties.models import Counterparty
 
 
@@ -7,9 +8,19 @@ class Bank(models.Model):
     name = models.CharField(max_length=255)
     iban_identifier = models.CharField(max_length=4)
 
+    def __str__(self):
+        return self.name
+
 
 class BankAccount(models.Model):
     iban = models.CharField(max_length=255, unique=True, null=True, blank=True)
     number = models.CharField(max_length=255, unique=True, null=True, blank=True)
     bank = models.ForeignKey(Bank, on_delete=models.PROTECT)
     counterparty = models.ForeignKey(Counterparty, on_delete=models.PROTECT, null=True, blank=True)
+
+    def __str__(self):
+        if len(self.iban) >= 1:
+            iban = Iban(self.iban)
+            return str(iban)
+        else:
+            return self.number
