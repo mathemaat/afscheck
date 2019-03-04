b8d17f684337986d41ade2a63c1ba326a0281a34
Marc Schwartz <m.s.schwartz@students.uu.nl>
2019-03-04 17:40:36 +0100

    Also parse the user's own bank account when importing a bank statement

--- classes/INGBankStatementParser.py
+++ classes/INGBankStatementParser.py
@@ -6,6 +6,7 @@ class INGBankStatementParser(CSVParser):
     # definieer kolomnummers
     KOLOM_DATUM = 0
     KOLOM_NAAM_OMSCHRIJVING = 1
+    KOLOM_REKENING = 2
     KOLOM_TEGENREKENING = 3
     KOLOM_CODE = 4
     KOLOM_AF_BIJ = 5
@@ -17,6 +18,7 @@ class INGBankStatementParser(CSVParser):
         self.column_definitions = {
             self.KOLOM_DATUM: CSVParser.TYPE_DATE,
             self.KOLOM_NAAM_OMSCHRIJVING: CSVParser.TYPE_STRING,
+            self.KOLOM_REKENING: CSVParser.TYPE_IBAN,
             self.KOLOM_TEGENREKENING: CSVParser.TYPE_STRING,
             self.KOLOM_CODE: CSVParser.TYPE_STRING,
             self.KOLOM_AF_BIJ: CSVParser.TYPE_STRING,
