67ff2729d8b9b063d0f2b3a4763371b05a904c4d
Marc Schwartz <m.s.schwartz@students.uu.nl>
2019-03-10 11:04:00 +0100

    Move class INGBankStatementParser

--- import/views.py
+++ import/views.py
@@ -1,7 +1,7 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 
-from classes.INGBankStatementParser import INGBankStatementParser
+from .utils import INGBankStatementParser
 
 
 def index(request):
