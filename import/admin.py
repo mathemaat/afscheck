b85088835e569828fff7440027749857c9790e78
Marc Schwartz <m.s.schwartz@students.uu.nl>
2019-03-10 09:35:09 +0100

    Add ING mutation types to the admin site

--- import/admin.py
+++ import/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import INGMutationType
+
+
+class INGMutationTypeAdmin(admin.ModelAdmin):
+    list_display = ('description', 'mutation_type')
+
+admin.site.register(INGMutationType, INGMutationTypeAdmin)
--- import/models.py
+++ import/models.py
@@ -7,5 +7,9 @@ class INGMutationType(models.Model):
     description = models.CharField(max_length=255)
     mutation_type = models.ForeignKey(MutationType, on_delete=models.PROTECT)
 
+    class Meta:
+        verbose_name = "ING mutation type"
+        verbose_name_plural = "ING mutation types"
+
     def __str__(self):
         return self.description
