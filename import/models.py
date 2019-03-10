3237bdeeace640e5b8612e7ef1a072e5c326490a
Marc Schwartz <m.s.schwartz@students.uu.nl>
2019-03-10 11:10:29 +0100

    Add model ContraAccount

--- import/models.py
+++ import/models.py
@@ -1,6 +1,24 @@
 from django.db import models
 
+from bank_accounts.models import BankAccount
 from bank_transactions.models import MutationType
+from counterparties.models import Counterparty
+
+
+class ContraAccount(models.Model):
+    description = models.CharField(max_length=255)
+    bank_account_number = models.CharField(max_length=255)
+    counterparty = models.ForeignKey(Counterparty, on_delete=models.PROTECT)
+    bank_account = models.ForeignKey(BankAccount, on_delete=models.PROTECT, null=True, blank=True)
+
+    class Meta:
+        unique_together = (("description", "bank_account_number"),)
+
+    def __str__(self):
+        if len(self.bank_account_number) >= 1:
+            return '%s (%s)' % (self.description, self.bank_account_number)
+        else:
+            return self.description
 
 
 class INGMutationType(models.Model):
